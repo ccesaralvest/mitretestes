af7f8007dd1ce9835a57df79eb9347110419ccfa
Filipe Melo <filipexpert63@gmail.com>
2025-03-05 13:40:05 -0300

    Mudança na lógica de páginas, eventos e fale conosco

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import ImageCarousel from "@/components/sections/6-ImageCarousel";
 import Header from "@/components/sections/0-Header";
 import Hero from "@/components/sections/1-Hero";
@@ -6,38 +8,87 @@ import Services from "@/components/sections/5-Services";
 import Tribeca from "@/components/sections/2-Tribeca";
 import Professionals from "@/components/sections/3-Professionals";
 import PlantasResidenciais from "@/components/sections/7-PlantasResidenciais";
-/* import Localizacao from "@/components/sections/8-Localizacao"; */
 import Contact from "@/components/sections/9-Contact";
 import Footer from "@/components/sections/10-Footer";
 import FloorPlan from "@/components/sections/6-b-Floor";
 import { GoogleTagManager } from "@next/third-parties/google";
-
 import ContactModal from "@/components/ui/ContactModal";
- 
 import LocationItemOutput from "@/components/sections/9-b-Gastronomia";
 import EventCardComponent from "@/components/sections/12-Event";
 import LocalCity from "@/components/sections/8-LocalCity";
 
 export default function Home() {
+  const [showEventOnly, setShowEventOnly] = useState(false);
+
+  useEffect(() => {
+    const checkHash = () => {
+      if (typeof window !== "undefined") {
+        setShowEventOnly(window.location.hash === "#eventCard" || window.location.hash === "#contact");
+      }
+    };
+
+    checkHash();
+    window.addEventListener("hashchange", checkHash);
+
+    return () => {
+      window.removeEventListener("hashchange", checkHash);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (showEventOnly) {
+      setTimeout(() => {
+        if (window.location.hash === "#contact") {
+          document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" });
+        }
+        if (window.location.hash === "#eventCard") {
+          document.getElementById("eventCard")?.scrollIntoView({ behavior: "smooth" });
+        }
+     
+        
+      
+      }, 500);
+    }
+  }, [showEventOnly]);
+
+  
+  useEffect(() => {
+    if (!showEventOnly) {
+      setTimeout(() => {
+        const currentHash = window.location.hash;
+        if (currentHash) {
+          document.getElementById(currentHash.substring(1))?.scrollIntoView({ behavior: "smooth" });
+        }
+      }, 500);
+    }
+  }, [showEventOnly]);
+
   return (
     <>
       <GoogleTagManager gtmId="GTM-WH8G73M8" />
       <ContactModal />
       <Header />
-      <Hero />
-      <Tribeca />
-      <Professionals />
-      <VideoPreview />
-      <ImageCarousel />
-      <Services />
-      <FloorPlan />
-      <PlantasResidenciais />
-      {/* Componente antigo. */}
-      
-      <LocalCity />
-      <EventCardComponent />
-      <LocationItemOutput />
-      <Contact />
+
+      {showEventOnly ? (
+        <>
+          <EventCardComponent />
+          <Contact />
+        </>
+      ) : (
+        <>
+          <Hero />
+          <Tribeca />
+          <Professionals />
+          <VideoPreview />
+          <ImageCarousel />
+          <Services />
+          <FloorPlan />
+          <PlantasResidenciais />
+          <LocalCity />
+          <LocationItemOutput />
+        </>
+      )}
+
       <Footer />
     </>
   );
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -27,26 +27,38 @@ export default function Menu() {
     { text: "CONTATO", href: "#contact" },
   ];
 
+  const handleMenuClick = (event: React.MouseEvent<HTMLAnchorElement>, href: any) => {
+    event.preventDefault();
+
+    if (window.location.hash === "#eventCard" || window.location.hash === "#contact") {
+      setTimeout(() => {
+        history.replaceState(null, "", window.location.pathname);
+        setTimeout(() => {
+          window.location.hash = href;
+          document.getElementById(href.substring(1))?.scrollIntoView({ behavior: "smooth" });
+        }, 200);
+      }, 500);
+    } else {
+      window.location.hash = href;
+      document.getElementById(href.substring(1))?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div>
       <div>
-        <ul className="sm:flex justify-center items-center font-unineue text-black uppercase  header-ul-primary hidden">
+        <ul className="sm:flex justify-center items-center font-unineue text-black uppercase header-ul-primary hidden">
           {menuItems.map((menu, index) => (
             <React.Fragment key={index}>
               <li
                 className={`group relative text-sm sm:text-[.550em] md:text-[.700em] lg:text-[.875em] px-2 cursor-pointer transition-transform duration-300 hover:scale-[1.04] before:w-0 hover:before:content-[''] hover:before:absolute hover:before:bottom-0 hover:before:left-1/2 hover:before:-translate-x-1/2 before:border-b before:border-transparent before:transition-all before:duration-500 hover:before:w-[90%] hover:before:border-b-foreground`}
               >
-                <a
-                  href={menu.href}
-                  className="flex justify-center items-center"
-                >
+                <a href={menu.href} className="flex justify-center items-center" onClick={(event) => handleMenuClick(event, menu.href)}>
                   {menu.text}
                 </a>
               </li>
 
-              {index < menuItems.length - 1 && (
-                <span className={`text-black`}>|</span>
-              )}
+              {index < menuItems.length - 1 && <span className={`text-black`}>|</span>}
             </React.Fragment>
           ))}
         </ul>
@@ -54,18 +66,14 @@ export default function Menu() {
       <div className="sm:hidden mt-4">
         <DropdownMenu onOpenChange={(isOpen) => setOpen(isOpen)}>
           <DropdownMenuTrigger asChild className="bg-foreground p-[2px] mr-8">
-            {open ? (
-              <ChevronDown className="text-white size-[27px]" />
-            ) : (
-              <MenuIcon className="text-white size-[27px]" />
-            )}
+            {open ? <ChevronDown className="text-white size-[27px]" /> : <MenuIcon className="text-white size-[27px]" />}
           </DropdownMenuTrigger>
           <DropdownMenuContent className="mr-2">
             {menuItems.map((menu, index) => (
               <React.Fragment key={index}>
                 <DropdownMenuGroup>
                   <DropdownMenuItem>
-                    <a href={menu.href} className="flex items-center uppercase">
+                    <a href={menu.href} className="flex items-center uppercase" onClick={(event) => handleMenuClick(event, menu.href)}>
                       {menu.text}
                     </a>
                   </DropdownMenuItem>
