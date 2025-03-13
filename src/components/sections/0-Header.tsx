5529d5822ad40623b8cf8e43b065cd94d93f0fa9
Caio Timoteo <ccesaralvest@gmail.com>
2025-03-13 06:18:58 -0300

    Update 0-Header.tsx

--- src/components/sections/0-Header.tsx
+++ src/components/sections/0-Header.tsx
@@ -5,6 +5,7 @@ import Menu from "../Menu";
 const Header = () => {
   return (
     <header className="min-w-full shadow-[0_4px_6px_rgba(0,0,0,0.1)]">
+      TESTE
       <div className="flex justify-end sm:justify-center  sm:shadow-[0_4px_6px_rgba(0,0,0,0.1)] ">
         <Menu />
       </div>
